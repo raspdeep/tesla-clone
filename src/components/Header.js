9588a4b1cc5177d41f553f9ccff0357ccc2d11f7
Jdub <terchong@yahoo.com>
2021-11-22 17:49:08 +0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useSelector } from "react-redux";
+import { selectCars } from "../features/car/carSlice";
+
+function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  //console.log(cars);
+  return (
+    <Container>
+      <a href="http://localhost:3000/#">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href="http://localhost:3000/#" alt="">
+              {car}
+            </a>
+          ))}
+      </Menu>
+      <RightMenu>
+        <a href="http://localhost:3000/#" alt="">
+          Shop
+        </a>
+        <a href="http://localhost:3000/#" alt="">
+          Tesla Account
+        </a>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <MenuClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="http://localhost:3000/#" alt="">
+                {car}
+              </a>
+            </li>
+          ))}
+        <li>
+          <a href="http://localhost:3000">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="http://localhost:3000">Used Inventory</a>
+        </li>
+        <li>
+          <a href="http://localhost:3000">Trade-in</a>
+        </li>
+        <li>
+          <a href="http://localhost:3000">Cybertruck</a>
+        </li>
+        <li>
+          <a href="http://localhost:3000">Roadster</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  opacity: 80%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 200px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const MenuClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
